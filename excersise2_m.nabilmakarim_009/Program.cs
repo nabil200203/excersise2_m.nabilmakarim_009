eabc4881094f91d43bf0b94e919060b27f04c3a6
TC1009 <m.nabil.ft21@mail.umy.ac.id>
2022-10-31 16:21:29 +0700

    add class

---
+++ excersise2_m.nabilmakarim_009/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excersise2_m.nabilmakarim_009
+{
+    internal class Program
+    {
+        private int[] arr = new int[29];
+        private int cmp_count = 0;
+        private int mov_count = 0;
+
+        private int n;
+        void Main()
+        {
+        }
+    }
+}
