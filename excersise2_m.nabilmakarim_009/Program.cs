4773537b5ee9bd7e0ef6bd98f2b7e2316f2f9256
TC1009 <m.nabil.ft21@mail.umy.ac.id>
2022-10-31 16:33:45 +0700

    add int

--- excersise2_m.nabilmakarim_009/Program.cs
+++ excersise2_m.nabilmakarim_009/Program.cs
@@ -24,6 +24,16 @@ namespace excersise2_m.nabilmakarim_009
                 else
                     Console.WriteLine("\nArray can have maximum 29 elements \n");
             }
+            Console.WriteLine("\n=======================");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n=======================");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
         }
         static void Main(string[] args)
         {
